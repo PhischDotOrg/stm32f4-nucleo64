a5542435a8a4e036f20b75097333b03da25d3881
Philip Schulz <phs@phisch.org>
2020-07-11 15:21:39 +0200

    Move common Infrastructure Code into common/ repository.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 -*/
 #include <version.h>
 
+#include <common/Infrastructure.hpp>
+
 #include <stm32f4/PwrViaSTM32F4.hpp>
 #include <stm32f4/FlashViaSTM32F4.hpp>
 #include <stm32f4/RccViaSTM32F4.hpp>
@@ -74,11 +76,6 @@ static tasks::HeartbeatT<decltype(g_uart), decltype(g_led_green)>       heartbea
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-extern char stext, etext;
-extern char sdata, edata;
-extern char sbss, ebss;
-extern char bstack, estack;
-
 const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
 
 static_assert(SystemCoreClock               == 96 * 1000 * 1000,   "Expected System Clock to be at 96 MHz!");
@@ -147,92 +144,3 @@ bad:
 #if defined(__cplusplus)
 } /* extern "C" */
 #endif /* defined(__cplusplus) */
-
-/*******************************************************************************
- *
- ******************************************************************************/
-#if defined(__cplusplus)
-extern "C" {
-#endif /* defined (__cplusplus) */
-void
-halt(const char * const p_file, const unsigned p_line) {
-    g_uart.printf("%s(): %s : %d\r\n", __func__, p_file, p_line);
-
-    while (1) { };
-}
-
-void
-assert_failed(uint8_t *p_file, uint32_t p_line) {
-    halt(reinterpret_cast<char *>(p_file), p_line);
-}
-
-int
-usleep(unsigned p_usec) {
-    SysTick_Type *sysTick = reinterpret_cast<SysTick_Type *>(SysTick_BASE);
-
-    /*
-     * Halt SysTick, if already running. Also, store current SysTick status.
-     */
-    bool enabled = (sysTick->CTRL & SysTick_CTRL_ENABLE_Msk) != 0;
-    if (enabled) {
-        sysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
-    }
-
-    unsigned safeCtrl = sysTick->CTRL;
-    unsigned safeLoad = sysTick->LOAD;
-    unsigned safeVal  = sysTick->VAL;
-
-    /*
-     * Configure SysTick for 1ms Overflow, then wait for required number of
-     * milliseconds.
-     */
-    const unsigned ticksPerMs = pllCfg.getSysclkSpeedInHz() / 1000;
-    assert((ticksPerMs & 0x00FFFFFF) == ticksPerMs); 
-    unsigned waitMs = p_usec / 1000;
-
-    sysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;
-    sysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
-
-    sysTick->LOAD = ticksPerMs;
-    sysTick->VAL = ticksPerMs;
-    sysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-    while (waitMs > 0) {
-        while (!(sysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)) ;
-        waitMs--;
-    }
-    sysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
-
-    /*
-     * Configure SysTick for 1us Overflow, then wait for required number of
-     * microseconds.
-     */
-    const unsigned ticksPerUs = pllCfg.getSysclkSpeedInHz() / (1000 * 1000);
-    assert((ticksPerUs & 0x00FFFFFF) == ticksPerUs);
-    unsigned waitUs = p_usec & 1024; // Assumes 1ms = 1024us. Close enough.
-
-    sysTick->LOAD = ticksPerUs;
-    sysTick->VAL = ticksPerUs;
-    sysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-    while (waitUs > 0) {
-        while (!(sysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)) ;
-        waitUs--;
-    }
-    sysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
-
-    /*
-     * Restore SysTick status.
-     */
-    sysTick->VAL  = safeVal;
-    sysTick->LOAD = safeLoad;
-    sysTick->CTRL = safeCtrl;
-    if (enabled) {
-        sysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-    }
-    
-    return 0;
-}
-
-#if defined(__cplusplus)
-} /* extern "C" */
-#endif /* defined (__cplusplus) */
-
