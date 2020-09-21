102a2522ef27a2ad98f3a23f046219465405a06d
Philip Schulz <phs@phisch.org>
2020-09-21 19:59:54 +0200

    Update to STM32 Arch v2.1

--- main.cpp
+++ main.cpp
@@ -2,82 +2,155 @@
  * $Copyright$
 -*/
 #include <common/Infrastructure.hpp>
+#include <phisch/log.h>
 
-#include <stm32f4/PwrViaSTM32F4.hpp>
-#include <stm32f4/FlashViaSTM32F4.hpp>
-#include <stm32f4/RccViaSTM32F4.hpp>
-#include <stm32f4/ScbViaSTM32F4.hpp>
-#include <stm32f4/NvicViaSTM32F4.hpp>
+/* for vTaskStartScheduler */
+#include <FreeRTOS.h> 
+#include <FreeRTOS/include/task.h>
+
+#include <stm32/Cpu.hpp>
+
+#include <stm32/Pll.hpp>
+#include <stm32/Pwr.hpp>
+#include <stm32/Flash.hpp>
+#include <stm32/Gpio.hpp>
+#include <stm32/Rcc.hpp>
+#include <stm32/Scb.hpp>
+#include <stm32/Nvic.hpp>
 
 #include <gpio/GpioAccess.hpp>
-
 #include <gpio/GpioEngine.hpp>
 #include <gpio/GpioPin.hpp>
 
+#include <stm32/Uart.hpp>
 #include <uart/UartAccess.hpp>
 #include <uart/UartDevice.hpp>
 
 #include <tasks/Heartbeat.hpp>
 
+#include <stm32/dma/Engine.hpp>
+#include <stm32/dma/Channel.hpp>
+#include <stm32/dma/Stream.hpp>
+
+#include <stm32/Spi.hpp>
+#include <spi/SpiAccess.hpp>
+#include <spi/SpiDevice.hpp>
+#include <devices/Ws2812bStrip.hpp>
+
 /*******************************************************************************
  * System Devices
  ******************************************************************************/
-static const constexpr devices::PllConfigurationValuesT<devices::Stm32F407xx> pllCfgValues = {
-    .m_pllSource        = devices::Stm32F407xx::PllSource_t::e_PllSourceHSI,
-    .m_hseSpeedInHz     = 8 * 1000 * 1000,
+static const constexpr stm32::PllCfg pllCfg = {
+    .m_pllSource        = stm32::PllCfg::PllSource_t::e_PllSourceHSI,
+    .m_hseSpeedInHz     = 0,
     .m_pllM             = 8,
     .m_pllN             = 192,
-    .m_pllP             = devices::Stm32F407xx::PllP_t::e_PllP_Div4,
-    .m_pllQ             = devices::Stm32F407xx::PllQ_t::e_PllQ_Div8,
-    .m_sysclkSource     = devices::Stm32F407xx::SysclkSource_t::e_SysclkPLL,
-    .m_ahbPrescaler     = devices::Stm32F407xx::AHBPrescaler_t::e_AHBPrescaler_None,
-    .m_apb1Prescaler    = devices::Stm32F407xx::APBPrescaler_t::e_APBPrescaler_Div2,
-    .m_apb2Prescaler    = devices::Stm32F407xx::APBPrescaler_t::e_APBPrescaler_None
+    .m_pllP             = stm32::PllCfg::PllP_t::e_PllP_Div4,
+    .m_pllQ             = stm32::PllCfg::PllQ_t::e_PllQ_Div8,
+    .m_sysclkSource     = stm32::PllCfg::SysclkSource_t::e_SysclkPLL,
+    .m_ahbPrescaler     = stm32::PllCfg::AHBPrescaler_t::e_AHBPrescaler_None,
+    .m_apb1Prescaler    = stm32::PllCfg::APBPrescaler_t::e_APBPrescaler_Div2,
+    .m_apb2Prescaler    = stm32::PllCfg::APBPrescaler_t::e_APBPrescaler_None
 };
 
-static const constexpr devices::PllConfigurationInterfaceT<decltype(pllCfgValues)> pllCfg(pllCfgValues);
+static stm32::Scb                       scb(SCB);
+static stm32::Nvic                      nvic(NVIC, scb);
 
-static devices::PwrViaSTM32F4           pwr(PWR);
-static devices::FlashViaSTM32F4         flash(FLASH);
-static devices::RccViaSTM32F4           rcc(RCC, pllCfg, flash, pwr);
-static devices::ScbViaSTM32F4           scb(SCB);
-static devices::NvicViaSTM32F4          nvic(NVIC, scb);
+static stm32::Pwr                       pwr(PWR);
+static stm32::Flash                     flash(FLASH);
+static stm32::Rcc                       rcc(RCC, pllCfg, flash, pwr);
 
 /*******************************************************************************
  * GPIO Engine Handlers 
  ******************************************************************************/
-static gpio::GpioAccessViaSTM32F4_GpioA gpio_A(rcc);
+static stm32::Gpio::A                   gpio_A(rcc);
 static gpio::GpioEngine                 gpio_engine_A(&gpio_A);
+
+static stm32::Gpio::B                   gpio_B(rcc);
+static gpio::GpioEngine                 gpio_engine_B(&gpio_B);
 
 /*******************************************************************************
  * LEDs
  ******************************************************************************/
-static gpio::PinT<decltype(gpio_engine_A)>  g_led_green(&gpio_engine_A, 5);
+static gpio::AlternateFnPin             g_mco1(gpio_engine_A, 8);
+static gpio::DigitalOutPin              g_led_green(gpio_engine_A, 5);
 
 /*******************************************************************************
  * UART
  ******************************************************************************/
-static gpio::PinT<decltype(gpio_engine_A)>  uart_tx(&gpio_engine_A, 2);
-static gpio::PinT<decltype(gpio_engine_A)>  uart_rx(&gpio_engine_A, 3);
-static uart::UartAccessSTM32F4_Uart2        uart_access(rcc, uart_rx, uart_tx);
-uart::UartDevice                            g_uart(&uart_access);
+static gpio::AlternateFnPin             uart_tx(gpio_engine_A, 2);
+static gpio::AlternateFnPin             uart_rx(gpio_engine_A, 3);
+static stm32::Uart::Usart2<gpio::AlternateFnPin>    uart_access(rcc, uart_rx, uart_tx);
+uart::UartDevice                        g_uart(&uart_access);
+
+/*******************************************************************************
+ * DMA Engines
+ ******************************************************************************/
+static stm32::Dma::Engine1  dma_1(rcc);
+static stm32::Dma::Engine2  dma_2(rcc);
+
+/*******************************************************************************
+ * DMA Streams and Channels
+ ******************************************************************************/
+static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 3>                  ws2812b_spiTxDmaStream(nvic, dma_2);
+static stm32::dma::ChannelT<decltype(ws2812b_spiTxDmaStream)>   ws2812b_spiTxDmaChannel(ws2812b_spiTxDmaStream, 3);
+
+static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 2>                  ws2812b_spiRxDmaStream(nvic, dma_2);
+static stm32::dma::ChannelT<decltype(ws2812b_spiRxDmaStream)>   ws2812b_spiRxDmaChannel(ws2812b_spiRxDmaStream, 3);
+
+/*******************************************************************************
+ * WS2812b Strip
+ ******************************************************************************/
+static gpio::AlternateFnPin spi_sclk(gpio_engine_B, 3);
+static gpio::AlternateFnPin spi_nsel(gpio_engine_A, 15);
+static gpio::AlternateFnPin spi_mosi(gpio_engine_B, 5);
+static gpio::AlternateFnPin spi_miso(gpio_engine_B, 4);
+
+static stm32::Spi::Spi1<
+    decltype(ws2812b_spiTxDmaChannel),
+    decltype(ws2812b_spiRxDmaChannel),
+    gpio::AlternateFnPin
+>
+ws2812b_spibus( /* p_rcc = */ rcc,
+                                        /* p_txDmaChannel = */ ws2812b_spiTxDmaChannel,
+                                        /* p_rxDmaChannel = */ ws2812b_spiRxDmaChannel,
+                                        /* p_sclk = */ spi_sclk,
+                                        /* p_nsel = */ spi_nsel,
+                                        /* p_mosi = */ spi_mosi,
+                                        /* p_miso = */ spi_miso,
+                                        /* p_prescaler = */ decltype(ws2812b_spibus)::e_SpiPrescaler2048);
+
+static spi::DeviceT<decltype(ws2812b_spibus)>               ws2812b_spidev(&ws2812b_spibus);
+static devices::Ws2812bStripT<1, decltype(ws2812b_spidev)>  ws2812bStrip(ws2812b_spidev);
 
 /*******************************************************************************
  * Tasks
  ******************************************************************************/
-static tasks::HeartbeatT<decltype(g_uart), decltype(g_led_green)>       heartbeat_gn("hrtbt_g", g_uart, g_led_green, 3, 500);
+static tasks::HeartbeatT<decltype(g_led_green)> heartbeat_gn("hrtbt_g", g_led_green, 3, 500);
+
+#if 0
+/* FIXME Only for testing the WS2812B Driver via SPI and DMA. */
+void
+toggleLed(bool p_status) {
+  if (p_status) {
+    ws2812bStrip.setPixel(0, FastLED::CRGB(0x0F0000));
+  } else {
+    ws2812bStrip.setPixel(0, FastLED::CRGB(0x000F00));
+  }
+  ws2812bStrip.show();
+}
+#endif
 
 /*******************************************************************************
  *
  ******************************************************************************/
-extern "C" const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
+const uint32_t SystemCoreClock = pllCfg.getSysclkSpeedInHz();
 
+static_assert(pllCfg.isValid() == true,                            "PLL Configuration is not valid!");
 static_assert(SystemCoreClock               == 96 * 1000 * 1000,   "Expected System Clock to be at 96 MHz!");
 static_assert(pllCfg.getAhbSpeedInHz()      == 96 * 1000 * 1000,   "Expected AHB to be running at 96 MHz!");
 static_assert(pllCfg.getApb1SpeedInHz()     == 48 * 1000 * 1000,   "Expected APB1 to be running at 48 MHz!");
 static_assert(pllCfg.getApb2SpeedInHz()     == 96 * 1000 * 1000,   "Expected APB2 to be running at 96 MHz!");
-
-static_assert(pllCfg.isValid(pllCfg) == true,                       "PLL Configuration is not valid!");
 
 /*******************************************************************************
  *
@@ -86,16 +159,16 @@ static_assert(pllCfg.isValid(pllCfg) == true,                       "PLL Configu
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-void
+int
 main(void) {
-    g_led_green.enable(gpio::GpioAccessViaSTM32F4::e_Output, gpio::GpioAccessViaSTM32F4::e_None, gpio::GpioAccessViaSTM32F4::e_Gpio);
+    // rcc.setMCO(g_mco1, decltype(rcc)::MCO1Output_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_5);
 
-    uart_access.setBaudRate(decltype(uart_access)::e_BaudRate_115200);
+    uart_access.setBaudRate(decltype(uart_access)::BaudRate_e::e_115200);
 
-    constexpr unsigned sysclk = pllCfg.getSysclkSpeedInHz() / 1000;
-    constexpr unsigned ahb    = pllCfg.getAhbSpeedInHz() / 1000;
-    constexpr unsigned apb1   = pllCfg.getApb1SpeedInHz() / 1000;
-    constexpr unsigned apb2   = pllCfg.getApb2SpeedInHz() / 1000;
+    const unsigned sysclk = pllCfg.getSysclkSpeedInHz() / 1000;
+    const unsigned ahb    = pllCfg.getAhbSpeedInHz() / 1000;
+    const unsigned apb1   = pllCfg.getApb1SpeedInHz() / 1000;
+    const unsigned apb2   = pllCfg.getApb2SpeedInHz() / 1000;
 
     PrintStartupMessage(sysclk, ahb, apb1, apb2);
 
@@ -110,6 +183,26 @@ main(void) {
 bad:
     PHISCH_LOG("FATAL ERROR!\r\n");
     while (1) ;
+
+    return (0);
+}
+
+/*******************************************************************************
+ * Interrupt Handlers 
+ ******************************************************************************/
+void
+SPI1_IRQHandler(void) {
+    ws2812b_spibus.handleIrq();
+}
+
+void
+DMA2_Stream2_IRQHandler(void) {
+    ws2812b_spiRxDmaStream.handleIrq();
+}
+
+void
+DMA2_Stream3_IRQHandler(void) {
+    ws2812b_spiTxDmaStream.handleIrq();
 }
 
 #if defined(__cplusplus)
