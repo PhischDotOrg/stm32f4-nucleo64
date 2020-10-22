832997392d9cfb9f883f95a9fd2a403997023166
Philip Schulz <phs@phisch.org>
2020-10-22 20:04:58 +0200

    Jenkins Support.

--- main.cpp
+++ main.cpp
@@ -28,9 +28,11 @@
 
 #include <tasks/Heartbeat.hpp>
 
-#include <stm32/dma/Engine.hpp>
-#include <stm32/dma/Channel.hpp>
-#include <stm32/dma/Stream.hpp>
+#if SPI_VIA_DMA
+#include <stm32f4/dma/Engine.hpp>
+#include <stm32f4/dma/Channel.hpp>
+#include <stm32f4/dma/Stream.hpp>
+#endif
 
 #include <stm32/Spi.hpp>
 #include <spi/SpiAccess.hpp>
@@ -69,6 +71,9 @@ static gpio::GpioEngine                 gpio_engine_A(&gpio_A);
 static stm32::Gpio::B                   gpio_B(rcc);
 static gpio::GpioEngine                 gpio_engine_B(&gpio_B);
 
+static stm32::Gpio::C                   gpio_C(rcc);
+static gpio::GpioEngine                 gpio_engine_C(&gpio_C);
+
 /*******************************************************************************
  * LEDs
  ******************************************************************************/
@@ -83,6 +88,7 @@ static gpio::AlternateFnPin             uart_rx(gpio_engine_A, 3);
 static stm32::Uart::Usart2<gpio::AlternateFnPin>    uart_access(rcc, uart_rx, uart_tx);
 uart::UartDevice                        g_uart(&uart_access);
 
+#if SPI_VIA_DMA
 /*******************************************************************************
  * DMA Engines
  ******************************************************************************/
@@ -92,33 +98,43 @@ static stm32::Dma::Engine2  dma_2(rcc);
 /*******************************************************************************
  * DMA Streams and Channels
  ******************************************************************************/
-static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 3>                  ws2812b_spiTxDmaStream(nvic, dma_2);
+static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 3>  ws2812b_spiTxDmaStream(nvic, dma_2);
 static stm32::dma::ChannelT<decltype(ws2812b_spiTxDmaStream)>   ws2812b_spiTxDmaChannel(ws2812b_spiTxDmaStream, 3);
 
-static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 2>                  ws2812b_spiRxDmaStream(nvic, dma_2);
+static stm32::dma::StreamT<decltype(nvic), decltype(dma_2), 2>  ws2812b_spiRxDmaStream(nvic, dma_2);
 static stm32::dma::ChannelT<decltype(ws2812b_spiRxDmaStream)>   ws2812b_spiRxDmaChannel(ws2812b_spiRxDmaStream, 3);
+#endif
 
 /*******************************************************************************
  * WS2812b Strip
  ******************************************************************************/
 static gpio::AlternateFnPin spi_sclk(gpio_engine_B, 3);
 static gpio::AlternateFnPin spi_nsel(gpio_engine_A, 15);
-static gpio::AlternateFnPin spi_mosi(gpio_engine_B, 5);
+static gpio::AlternateFnPin spi_mosi(gpio_engine_C, 3);
 static gpio::AlternateFnPin spi_miso(gpio_engine_B, 4);
 
-static stm32::Spi::Spi1<
+#if defined(SPI_VIA_DMA)
+static stm32::Spi::DmaSpi1<
+    gpio::AlternateFnPin,
     decltype(ws2812b_spiTxDmaChannel),
-    decltype(ws2812b_spiRxDmaChannel),
+    decltype(ws2812b_spiRxDmaChannel)
+>
+#else
+static stm32::Spi::Spi2<
     gpio::AlternateFnPin
 >
+#endif
 ws2812b_spibus( /* p_rcc = */ rcc,
-                                        /* p_txDmaChannel = */ ws2812b_spiTxDmaChannel,
-                                        /* p_rxDmaChannel = */ ws2812b_spiRxDmaChannel,
-                                        /* p_sclk = */ spi_sclk,
-                                        /* p_nsel = */ spi_nsel,
-                                        /* p_mosi = */ spi_mosi,
-                                        /* p_miso = */ spi_miso,
-                                        /* p_prescaler = */ decltype(ws2812b_spibus)::e_SpiPrescaler2048);
+            #if defined(SPI_VIA_DMA)
+                /* p_txDmaChannel = */ ws2812b_spiTxDmaChannel,
+                /* p_rxDmaChannel = */ ws2812b_spiRxDmaChannel,
+            #endif
+                /* p_sclk = */ spi_sclk,
+                /* p_nsel = */ spi_nsel,
+                /* p_mosi = */ spi_mosi,
+                /* p_miso = */ spi_miso,
+                /* p_prescaler = */ decltype(ws2812b_spibus)::BaudRatePrescaler_e::e_SpiPrescaler16
+);
 
 static spi::DeviceT<decltype(ws2812b_spibus)>               ws2812b_spidev(&ws2812b_spibus);
 static devices::Ws2812bStripT<1, decltype(ws2812b_spidev)>  ws2812bStrip(ws2812b_spidev);
@@ -128,17 +144,34 @@ static devices::Ws2812bStripT<1, decltype(ws2812b_spidev)>  ws2812bStrip(ws2812b
  ******************************************************************************/
 static tasks::HeartbeatT<decltype(g_led_green)> heartbeat_gn("hrtbt_g", g_led_green, 3, 500);
 
-#if 0
+#if 1
 /* FIXME Only for testing the WS2812B Driver via SPI and DMA. */
-void
-toggleLed(bool p_status) {
-  if (p_status) {
-    ws2812bStrip.setPixel(0, FastLED::CRGB(0x0F0000));
-  } else {
-    ws2812bStrip.setPixel(0, FastLED::CRGB(0x000F00));
-  }
-  ws2812bStrip.show();
+static bool led_status = false;
+
+int
+toggleLed(void *p_data) {
+    assert(p_data != nullptr);
+
+    bool *status = static_cast<bool *>(p_data);
+
+    Pixel::RGB color;
+    if (*status) {
+        color = Pixel::RGB(0x40'00'00);
+    } else {
+        color = Pixel::RGB(0x00'40'00);
+    }
+
+    for (unsigned idx = 0; idx < ws2812bStrip.SIZE; idx++) {
+        ws2812bStrip.setPixel(idx, color);
+    }
+    ws2812bStrip.show();
+
+    *status = !(*status);
+
+    return (0);
 }
+
+static tasks::PeriodicCallback  task_500ms("t_500ms", 2, 500, &toggleLed, &led_status);
 #endif
 
 /*******************************************************************************
@@ -161,9 +194,9 @@ extern "C" {
 
 int
 main(void) {
-    // rcc.setMCO(g_mco1, decltype(rcc)::MCO1Output_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_5);
+    rcc.setMCO(g_mco1, decltype(rcc)::MCO1Output_e::e_PLL, decltype(rcc)::MCOPrescaler_t::e_MCOPre_5);
 
-    uart_access.setBaudRate(decltype(uart_access)::BaudRate_e::e_115200);
+    uart_access.setBaudRate(decltype(uart_access)::BaudRate_e::e_230400);
 
     const unsigned sysclk = pllCfg.getSysclkSpeedInHz() / 1000;
     const unsigned ahb    = pllCfg.getAhbSpeedInHz() / 1000;
@@ -187,6 +220,7 @@ bad:
     return (0);
 }
 
+#if SPI_VIA_DMA
 /*******************************************************************************
  * Interrupt Handlers 
  ******************************************************************************/
@@ -204,6 +238,7 @@ void
 DMA2_Stream3_IRQHandler(void) {
     ws2812b_spiTxDmaStream.handleIrq();
 }
+#endif
 
 #if defined(__cplusplus)
 } /* extern "C" */
